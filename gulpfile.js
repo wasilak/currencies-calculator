ef20d10eaf4f96cbeb92459aef7e78c74d183240
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2016-05-25 23:57:24 +0200

    added few tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,46 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+
+var livereload = require('gulp-livereload');
+
+var postcss = require('gulp-postcss');
+var reporter = require('postcss-reporter');
+var stylelint = require('stylelint');
+
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
-  return gulp.src('./js/src/**/*.ts')
+var files = [
+  './js/src/**/*.ts',
+  './index.php',
+  './proxy.php',
+  './css/style.css'
+];
+
+gulp.task('default', function () {
+  return gulp.src(files)
 		.pipe(ts(tsProject))
-    .pipe(gulp.dest('./js/'));
+    .pipe(gulp.dest('./js/'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(files, ['default']);
+});
+
+gulp.task('lint:css', function() {
+  return gulp.src('css/**/*.css')
+    .pipe(postcss([
+        stylelint({ /* options */ }),
+        reporter({ clearMessages: true })
+    ]));
+});
+
+gulp.task('lint:js', function() {
+  return gulp.src('./js/app/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
 });
