a926daf2c9b8e7d33983a5818cd7e85018d0a27b
Piotr Boruc <piotr.m.boruc@gmail.com>
2021-05-03 22:43:28 +0200

    removed obsolete files

--- src/public/js/app/app.js
+++
@@ -1,8 +0,0 @@
-var currenciesCalulator = angular.module("currenciesCalulator", []);
-currenciesCalulator.controller("MainController", ["$scope", "$http", "$interval", function ($scope, $http, $interval) { return new Application.Controllers.MainController($scope, $http, $interval); }]);
-;
-var walutaPL = {
-    code: 'PLN',
-    currency: "złoty polski",
-    mid: '1.00',
-};
